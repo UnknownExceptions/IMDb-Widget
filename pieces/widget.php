374c3f71cf179ec8a57ec6acd9c7396728e66a3f
Luís Soares <lsoares@gmail.com>
2015-05-16 14:48:40 +0100

    ratings v0

--- pieces/widget.php
+++ pieces/widget.php
@@ -8,13 +8,15 @@
 		<img src='http://img4.wikia.nocookie.net/__cb20130124112826/logopedia/images/8/8e/IMDB.png'
 			 style="max-height: 20px;"/>
 		<a href="<?= $info->profileUrl ?>" target="_blank" title="View profile" style="display: block;">
-			<img src="<?= $info->avatar ?>"/>
-
+			<img src="<?= $info->avatar ?>" style="border-radius: 3px;" />
 			<h3><?= $info->nick ?></h3>
 		</a>
 
 		<div style="font-size: 80%;">
 			<?= $info->memberSince ?>
+		</div>
+		<div style="font-size: 80%; color: darkgrey">
+			<?= $info->bio ?>
 		</div>
 		<a href="<?= $info->ratingsUrlRss ?>" target="_blank" title="Ratings RSS">
 			<img src='http://www.intrepidmuseum.org/App_Themes/Intrepid/images/rss_logo.gif'/>
