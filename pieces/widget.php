8f5ec1791ac78a99047c8a6dadd55274e1b915c8
Henrique Dias <hacdias@gmail.com>
2015-05-19 20:46:37 +0100

    resolve #2

--- pieces/widget.php
+++ pieces/widget.php
@@ -1,96 +1,72 @@
-<aside class="widget imdbWidget" style="font-family: Verdana; color: black;">
+<aside class="widget">
 
-    <?php if (isset($config['title'])) : ?>
-        <?php echo $before_title . $config['title'] . $after_title; ?>
-    <?php endif; ?>
+	<?php if ( isset( $config['title'] ) ) : ?>
+		<?php echo $before_title . $config['title'] . $after_title; ?>
+	<?php endif; ?>
 
-    <style>
-        .histogram-horizontal {
-            position: relative;
-            height: 100px;
-        }
+	<div class="imdb-widget">
+		<a href="<?= $info->url ?>" target="_blank" title="View profile" class="imdb-avatar-link">
+			<img src="<?= $info->avatar ?>" class="imdb-avatar"/>
+			<img src='<?= IMDB_PLUGIN_URL . 'css/imdb-logo.png'; ?>' class="imdb-logo"/>
+		</a>
 
-        .histogram-horizontal .bar.shadow {
-            box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
-        }
-        .histogram-horizontal .bar {
-            background: #ddd;
-            position: absolute;
-            bottom: 0;
-            display: block;
-        }
-        .bar:hover {
-            background: #99d;
-          }
-    </style>
-    <div>
-        <a href="<?= $info->url ?>" target="_blank" title="View profile"
-           style="display: inline-block; width: 45%; vertical-align: top; position: relative;border-radius: 3px;">
-            <img src="<?= $info->avatar ?>" style="width: 100%"/>
-            <img src='https://img4.wikia.nocookie.net/__cb20130124112826/logopedia/images/8/8e/IMDB.png'
-                 style="max-height: 20px; position: absolute; left: 0; bottom: 0; opacity: 0.8;"/>
-        </a>
-
-        <div style="display: inline-block; width: 50%; vertical-align: top;">
-			<span style="display: block;font-size: 115%;font-weight: bold;color: black; margin-bottom: 0px; line-height: 100%;">
+		<div class="imdb-info-box">
+			<span class="imdb-nick">
 				<?= $info->nick ?>
 			</span>
-            <div style="font-size: 9px; color: black; margin-top: 0">
-                <?= $info->memberSince ?>
-            </div>
-            <div style="font-size: 70%; color: darkgrey">
-                <?= $info->bio ?>
-            </div>
+
+			<div class="imdb-member-since">
+				<?= $info->memberSince ?>
+			</div>
+			<div class="imdb-bio">
+				<?= $info->bio ?>
+			</div>
 			<!-- BADGES -->
 			<?php
-				for ($i = 0; $i < count($info->badges); $i++) {
-					$badge = $info->badges[$i]; ?>
-			<?= $badge->logo ?>
+			for ( $i = 0; $i < count( $info->badges ); $i ++ ) {
+				$badge = $info->badges[ $i ]; ?>
+				<?= $badge->logo ?>
 			<?php } ?>
-        </div>
-    </div>
+		</div>
 
-    <!-- LATEST RATINGS -->
-    <div style="margin-bottom: 8px;">
-        <div style="font-weight: bold; margin-top: 8px; font-size: 80%;color:#A58500;">Latest ratings</div>
-        <?php
-        for ($i = 0; $i < count($info->ratings); $i++) {
-			$rating = $info->ratings[$i]; ?>
+		<!-- LATEST RATINGS -->
+		<div class="imdb-block imdb-ratings">
+			<div class="imdb-block-title">Latest ratings</div>
+			<?php for ( $i = 0; $i < count( $info->ratings ); $i ++ ) : ?>
+				<?php $rating = $info->ratings[ $i ]; ?>
+				<a target="_blank" href="<?= $info->baseUrl . $rating->link ?>"
+				   title="<?= $rating->title ?> (<?= $rating->rating ?>)" class="imdb-block-item-title">
+					<img src="<?= $rating->logo ?>" class="imdb-block-item-logo"/>
+				</a>
+			<?php endfor; ?>
 
-        <a target="_blank" href="<?= $info->baseUrl . $rating->link ?>"
-           title="<?= $rating->title ?> (<?= $rating->rating ?>)" style="display: inline-block;">
-            <img src="<?= $rating->logo ?>" style="height: 60px; margin: 1px; border-radius: 2px;"/>
-            <a/>
-        <?php } ?>
+			<a href="<?= $info->ratingsUrl ?>" target="_blank"
+			   title="See all user ratings" class="imdb-ratings-url">
+				<?= $info->ratingsCount; ?> »
+			</a>
+		</div>
 
-            <a href="<?= $info->ratingsUrl ?>" target="_blank"
-               title="See all user ratings" style="font-size: 70%;display: block;">
-                <?= $info->ratingsCount; ?> »
-            </a>
-    </div>
-	
-    <!-- WATCHLIST -->
-    <div style="margin-bottom: 8px;">
-        <div style="font-weight: bold; margin-top: 8px; font-size: 80%;color:#A58500;">Watchlist</div>
-        <?php
-        for ($i = 0; $i < count($info->watchlist); $i++) {
-			$watch = $info->watchlist[$i];
-        ?>
+		<!-- WATCHLIST -->
+		<div class="imdb-block imdb-watchlist">
+			<div class="imdb-block-title">Watchlist</div>
+			<?php for ( $i = 0; $i < count( $info->watchlist ); $i ++ ) : ?>
+				<?php $watch = $info->watchlist[ $i ]; ?>
+				<a target="_blank" href="<?= $info->baseUrl . $watch->link ?>"
+				   title="<?= $watch->title ?>" class="imdb-block-item-title">
+					<img src="<?= $watch->logo ?>" class="imdb-block-item-logo"/>
+				</a>
+			<?php endfor; ?>
 
-        <a target="_blank" href="<?= $info->baseUrl . $watch->link ?>"
-           title="<?= $watch->title ?>" style="display: inline-block;">
-            <img src="<?= $watch->logo ?>" style="height: 60px; margin: 2px; border-radius: 2px;"/>
-            <a/>
-            <?php } ?>
+			<a href="<?= $info->watchlistUrl ?>" target="_blank"
+			   title="See more" class="imdb-see-more">
+				See all »
+			</a>
+		</div>
 
-            <a href="<?= $info->watchlistUrl ?>" target="_blank"
-               title="See more" style="font-size: 70%;display: block;">
-                See all »
-            </a>
-    </div>
-    
-    <div class="histogram-horizontal">
-        <?= $info->ratingsHistogram ?>
-    </div>
+		<div class="imdb-histogram-horizontal">
+			<?= $info->ratingsHistogram ?>
+		</div>
+
+	</div>
 
 </aside>
