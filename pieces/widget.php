a830c119748d390a177be1a496f08d2f3a6bea19
Luís Soares <lsoares@gmail.com>
2015-05-22 17:43:31 +0100

    reviews!

--- pieces/widget.php
+++ pieces/widget.php
@@ -76,13 +76,13 @@
 			</a>
 		</div>
 		
-		<!-- USER LISTS -->
+		<!-- LISTS -->
 		<div class="imdb-user-lists">
 			<div class="imdb-block-title">Lists</div>
 			<?php for ( $i = 0; $i < count( $info->userLists ); $i ++ ) : ?>
 				<?php $list = $info->userLists[ $i ]; ?>
 				<a target="_blank" href="<?= $info->baseUrl . $list->link ?>"
-				   title="<?= $list->title ?>" class="imdb-block-item-title">
+				   title="List: <?= $list->title ?>" class="imdb-block-item-title">
 					<img src="<?= (substr($list->logo,0,1) == '/' ? $info->baseUrl : '') . $list->logo ?>" 
                                              class="imdb-block-item-small-logo" />
 					<?= $list->title ?>
@@ -95,7 +95,39 @@
 				See all »
 			</a>
 		</div>
-		
+                
+                
+                <!-- REVIEWS -->
+                <?php if (count( $info->reviews ) > 0) { ?>
+		<div class="imdb-user-reviews">
+			<div class="imdb-block-title">Reviews</div>
+			<?php for ( $i = 0; $i < count( $info->reviews ); $i ++ ) : ?>
+                            <?php $review = $info->reviews[ $i ]; ?>
+                            <div class="imdb-user-review">
+                                <div class="imdb-block-item-title">
+                                    <a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>" target="_blank">
+                                         <span title="<?= $review->movieTitle ?> <?= $review->movieYear ?>"><?= $review->movieTitle ?></span>
+                                    </a><?= $review->movieYear ?>
+                                </div>
+                                <div class="imdb-user-review-title"><?= $review->title ?></div>                              
+                                <div class="imdb-user-reviews-left ">
+                                   <a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>"
+                                      class="imdb-block-item-title" target="_blank">
+                                        <img src="<?= $review->movieLogo ?>" style="width: 45%; display: inline-block;" />
+                                   </a>
+                                </div>
+                                <div class="imdb-user-reviews-right">
+                                    <span><?= $review->text ?></span>                                
+                                    <span><?= $review->meta ?></span>
+                                </div>
+                            </div>
+                        <?php endfor; ?>
+			<a href="<?= $info->reviewsUrl ?>" target="_blank"
+			   title="See all reviews" class="imdb-widget-small-link">
+				See all »
+			</a>
+		</div>
+                <?php } ?>	
 
 		<!-- OVERLAY RATINGS -->
 		<div class="imdb-widget-charts">
--- plugin.php
+++ plugin.php
@@ -9,7 +9,6 @@
  * Network: true
  * License: GPL2 or later
  */
-
 // third-party libraries
 require_once( 'lib/htmlcompressor.php' );
 require_once( 'vendor/autoload.php' );
@@ -17,139 +16,152 @@ require_once( 'vendor/autoload.php' );
 use SmartScrapper\Parser;
 
 // prevent direct file access
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH'))
+{
+    exit;
 }
 
-class IMDb_Widget extends WP_Widget {
-	protected $widget_slug = 'imdb-widget';
+class IMDb_Widget extends WP_Widget
+{
 
-	private $options = array(
-		"title",
-		"userId"
-	);
+    protected $widget_slug = 'imdb-widget';
+    private $options = array(
+        "title",
+        "userId"
+    );
 
-	public function __construct() {
-		parent::__construct(
-			$this->get_widget_slug(),
-			__( 'IMDb Widget', $this->get_widget_slug() ),
-			array(
-				'classname'   => $this->get_widget_slug() . '-class',
-				'description' => __( 'A widget to show a small version of your IMDb profile.', $this->get_widget_slug() )
-			)
-		);
+    public function __construct()
+    {
+        parent::__construct(
+                $this->get_widget_slug(), __('IMDb Widget', $this->get_widget_slug()), array(
+            'classname' => $this->get_widget_slug() . '-class',
+            'description' => __('A widget to show a small version of your IMDb profile.', $this->get_widget_slug())
+                )
+        );
 
-		add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_styles' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_scripts' ) );
-	}
+        add_action('wp_enqueue_scripts', array($this, 'register_widget_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'register_widget_scripts'));
+    }
 
-	public function get_widget_slug() {
-		return $this->widget_slug;
-	}
+    public function get_widget_slug()
+    {
+        return $this->widget_slug;
+    }
 
-	public function form( $config ) {
-		$config = ! empty( $config ) ? unserialize( $config ) : array();
+    public function form($config)
+    {
+        $config = !empty($config) ? unserialize($config) : array();
 
-		foreach ( $this->options as $option ) {
-			${$option} = isset( $config[ $option ] ) ? $config[ $option ] : null;
-		}
+        foreach ($this->options as $option)
+        {
+            ${$option} = isset($config[$option]) ? $config[$option] : null;
+        }
 
-		ob_start( "HTMLCompressor" );
-		require 'pieces/options.php';
-		ob_end_flush();
-	}
+        ob_start("HTMLCompressor");
+        require 'pieces/options.php';
+        ob_end_flush();
+    }
 
-	public function update( $new_instance, $old_instance ) {
-		return serialize( $new_instance );
-	}
+    public function update($new_instance, $old_instance)
+    {
+        return serialize($new_instance);
+    }
 
-	public function widget( $args, $instance ) {
-		extract( $args, EXTR_SKIP );
-		$instance = ! empty( $instance ) ? unserialize( $instance ) : array();
+    public function widget($args, $instance)
+    {
+        extract($args, EXTR_SKIP);
+        $instance = !empty($instance) ? unserialize($instance) : array();
 
-		ob_start( "HTMLCompressor" );
+        ob_start("HTMLCompressor");
 
-		if ( ! isset( $instance['userId'] ) ) {
-			echo 'You need to first configure the plugin :)';
-		} else {
-			$info = $this->get_info( $instance['userId'] );
-			require 'pieces/widget.php';
-		}
-		ob_end_flush();
-	}
+        if (!isset($instance['userId']))
+        {
+            echo 'You need to first configure the plugin :)';
+        } else
+        {
+            $info = $this->get_info($instance['userId']);
+            require 'pieces/widget.php';
+        }
+        ob_end_flush();
+    }
 
-	private function get_info( $userId ) {
-		$info          = new Parser( 'http://www.imdb.com/' . 'user/' . $userId . '/' );
-		$info->baseUrl = 'http://www.imdb.com';
+    private function get_info($userId)
+    {
+        $info = new Parser('http://www.imdb.com/' . 'user/' . $userId . '/');
+        $info->baseUrl = 'http://www.imdb.com';
 
-		foreach (
-			array(
-				'ratings',
-				'boards',
-				'lists',
-				'watchlist',
-				'checkins',
-				'boards/sendpm',
-				'comments-index',
-				'#pollResponses'
-			) as $relativeUrl
-		) {
-			$cleanId                  = preg_replace( '/[^A-Za-z]/', '', $relativeUrl );
-			$info->{$cleanId . 'Url'} = $info->url . $relativeUrl;
-		}
+        foreach (
+        array('ratings', 'boards', 'lists', 'watchlist', 'checkins', 'boards/sendpm', 'comments-index', '#pollResponses') as $relativeUrl)
+        {
+            $cleanId = preg_replace('/[^A-Za-z]/', '', $relativeUrl);
+            $info->{$cleanId . 'Url'} = $info->url . $relativeUrl;
+        }
 
-		$info->text( 'nick', '.header h1' );
-		$info->text( 'avatar', '#avatar-frame img', 'src' );
-		$info->text( 'memberSince', '.header .timestamp' );
-//		$info->text( 'bio', '.header .biography' );
-		$info->text( 'ratingsCount', '.see-more a' );
-		$info->html( 'ratingsDistribution', '.overall .histogram-horizontal' );
-		$info->html( 'ratingsByYear', '.byYear .histogram-horizontal' );
-		$info->html( 'ratingsByYearLegend', '.byYear .legend' );
-		$info->html( 'ratingsTopRatedGenres', '.histogram-vertical' );
-//		$info->html( 'ratingsTopRatedYears', '.histogram-vertical' , 1 ); // TODO.
-		
-		$info->selectList( 'ratings', '.ratings .item' )
-		     ->with( 'link', 'a', 'href' )
-		     ->with( 'logo', 'a img', 'src' )
-		     ->with( 'title', '.title a' )
-		     ->with( 'rating', '.sub-item .only-rating' )
-		     ->save();
+        $info->text('nick', '.header h1');
+        $info->text('avatar', '#avatar-frame img', 'src');
+        $info->text('memberSince', '.header .timestamp');
+//        $info->text('bio', '.header .biography');
+//        $info->text('ratingsCount', '.see-more a');
+//        $info->html('ratingsDistribution', '.overall .histogram-horizontal');
+//        $info->html('ratingsByYear', '.byYear .histogram-horizontal');
+//        $info->html('ratingsByYearLegend', '.byYear .legend');
+//        $info->html('ratingsTopRatedGenres', '.histogram-vertical');
+//	 // $info->html( 'ratingsTopRatedYears', '.histogram-vertical' , 1 ); // TODO.
 
-		$info->selectList( 'badges', '.badges .badge-frame' )
-		     ->with( 'title', '.name' )
-		     ->with( 'value', '.value' )
-		     ->save();
+        $info->selectList('ratings', '.ratings .item')
+                ->with('link', 'a', 'href')
+                ->with('logo', 'a img', 'src')
+                ->with('title', '.title a')
+                ->with('rating', '.sub-item .only-rating')
+                ->save();
 
-		$info->selectList( 'watchlist', '.watchlist .item' )
-		     ->with( 'title', '.sub-item a' )
-		     ->with( 'link', 'a', 'href' )
-		     ->with( 'logo', 'a img', 'src' )
-		     ->save();
+        $info->selectList('badges', '.badges .badge-frame')
+                ->with('title', '.name')
+                ->with('value', '.value')
+                ->save();
 
-		$info->selectList( 'lists', '.lists .user-list' )
-		     ->with( 'title', '.list-name' )
-		     ->with( 'link', '.list-meta', 'href' )
-		     ->with( 'meta', '.list-meta' )
-		     ->save();
-		
-		$info->selectList( 'userLists', '.user-lists .user-list' )
-			->with( 'logo', 'img', 'src' )
-			->with( 'title', '.list-name')
-			->with( 'link', 'a' , 'href' )
-			->with( 'description', '.list-meta')
-			->save();
+        $info->selectList('watchlist', '.watchlist .item')
+                ->with('title', '.sub-item a')
+                ->with('link', 'a', 'href')
+                ->with('logo', 'a img', 'src')
+                ->save();
 
-		return $info;
-	}
+        $info->selectList('lists', '.lists .user-list')
+                ->with('title', '.list-name')
+                ->with('link', '.list-meta', 'href')
+                ->with('meta', '.list-meta')
+                ->save();
 
-	public function register_widget_styles() {
-		wp_enqueue_style( $this->get_widget_slug() . '-widget-styles', plugins_url( 'css/widget.css', __FILE__ ) );
-	}
+        $info->selectList('userLists', '.user-lists .user-list')
+                ->with('logo', 'img', 'src')
+                ->with('title', '.list-name')
+                ->with('link', 'a', 'href')
+                ->with('description', '.list-meta')
+                ->save();
 
-	public function register_widget_scripts() {
-		wp_enqueue_script( $this->get_widget_slug() . '-script', plugins_url( 'js/widget.js', __FILE__ ), array( 'jquery' ) );
-	}
+        $info->selectList('reviews', '.reviews .item')
+                ->with('movieLogo', '.image img', 'src')
+                ->with('movieLink', '.image a', 'href')
+                ->with('movieTitle', 'h3 a')
+                ->with('movieYear', 'h3 span')
+                ->with('title', 'h4')
+                ->with('meta', '.details')
+                ->with('text', 'p')
+                ->save();
+
+        return $info;
+    }
+
+    public function register_widget_styles()
+    {
+        wp_enqueue_style($this->get_widget_slug() . '-widget-styles', plugins_url('css/widget.css', __FILE__));
+    }
+
+    public function register_widget_scripts()
+    {
+        wp_enqueue_script($this->get_widget_slug() . '-script', plugins_url('js/widget.js', __FILE__), array('jquery'));
+    }
+
 }
 
-add_action( 'widgets_init', create_function( '', 'return register_widget("IMDb_Widget");' ) );
+add_action('widgets_init', create_function('', 'return register_widget("IMDb_Widget");'));
