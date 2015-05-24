772499c4ab3c2deb0e5c57705e14bf3b4864b3b4
Henrique Dias <hacdias@gmail.com>
2015-05-24 18:40:48 +0100

    remove </div>

--- pieces/widget.php
+++ pieces/widget.php
@@ -1,31 +1,32 @@
 <aside class="widget">
 
-    <?php if ( isset( $config['title'] ) ) : ?>
-	<?php echo $before_title . $config['title'] . $after_title; ?>
-    <?php endif; ?>
+	<?php if ( isset( $config['title'] ) ) : ?>
+		<?php echo $before_title . $config['title'] . $after_title; ?>
+	<?php endif; ?>
 
-    <div class="imdb-widget">
+	<div class="imdb-widget">
 		<div class="imdb-logo"></div>
-		
+
 		<a href="<?= $info->url ?>" target="_blank" title="View profile" class="imdb-nick">
 			<?= $info->nick ?>
 		</a>
+
 		<div class="imdb-member-since">
 			<?= $info->memberSince ?>
 		</div>
 
 		<div class="imdb-avatar-wrapper">
 			<a href="<?= $info->url ?>" target="_blank" title="View profile" class="imdb-avatar-link">
-				<img src="<?= $info->avatar ?>" class="imdb-avatar" />
+				<img src="<?= $info->avatar ?>" class="imdb-avatar"/>
 				<a class="imdb-widget-icon imdb-ratings-charts-message-link" href="<?= $info->boardssendpmUrl ?>"
 				   target="_blank" title="Send private message"></a>
 			</a>
 		</div>
 
-        <div class="imdb-info-box">
+		<div class="imdb-info-box">
 			<!-- BADGES -->
-            <?php
-            for ( $i = 0; $i < count( $info->badges ); $i ++ ) {
+			<?php
+			for ( $i = 0; $i < count( $info->badges ); $i ++ ) {
 				$badge = $info->badges[ $i ]; ?>
 				<div class="imdb-badge-info">
 					<a href="http://www.imdb.com/badge/" target="_blank">
@@ -33,22 +34,22 @@
 						<span><?= $badge->title ?></span>
 					</a>
 				</div>
-            <?php } ?>
+			<?php } ?>
 			<div class="imdb-bio">
 				<?= $info->bio ?>
 			</div>
-        </div>
-		
-		
-        <!-- LATEST RATINGS -->
-		<?php if (count( $info->ratings )) { ?>
-        <div class="imdb-block imdb-ratings">
-			<div class="imdb-block-title">Latest ratings
-				<a href="javascript:void(0);" class="imdb-ratings-charts-link imdb-widget-icon"
-				   title="Ratings charts"></a>
-			</div>
-			
-			<?php for ( $i = 0; $i < count( $info->ratings ); $i ++ ) : ?>
+		</div>
+
+
+		<!-- LATEST RATINGS -->
+		<?php if ( count( $info->ratings ) ) { ?>
+			<div class="imdb-block imdb-ratings">
+				<div class="imdb-block-title">Latest ratings
+					<a href="javascript:void(0);" class="imdb-ratings-charts-link imdb-widget-icon"
+					   title="Ratings charts"></a>
+				</div>
+
+				<?php for ( $i = 0; $i < count( $info->ratings ); $i ++ ) : ?>
 					<?php $rating = $info->ratings[ $i ]; ?>
 					<a target="_blank" href="<?= $info->baseUrl . $rating->link ?>"
 					   title="<?= $rating->title ?> (<?= $rating->rating ?> ★)" class="imdb-block-item-title">
@@ -57,152 +58,158 @@
 							<?= $rating->rating ?>
 						</span>
 					</a>
-			<?php endfor; ?>
+				<?php endfor; ?>
 
-			<a href="<?= $info->ratingsUrl ?>" target="_blank"
-			   title="See all user ratings" class="imdb-widget-small-link">
+				<a href="<?= $info->ratingsUrl ?>" target="_blank"
+				   title="See all user ratings" class="imdb-widget-small-link">
 					<?= $info->ratingsCount; ?> »
-			</a>
-        </div>
+				</a>
+			</div>
 		<?php } ?>
 
-        <!-- WATCHLIST -->
-		<?php if (count( $info->watchlist )) { ?>
-        <div class="imdb-block imdb-watchlist">
-			<div class="imdb-block-title">Watchlist</div>
-			<?php for ( $i = 0; $i < count( $info->watchlist ); $i ++ ) : ?>
+		<!-- WATCHLIST -->
+		<?php if ( count( $info->watchlist ) ) { ?>
+			<div class="imdb-block imdb-watchlist">
+				<div class="imdb-block-title">Watchlist</div>
+				<?php for ( $i = 0; $i < count( $info->watchlist ); $i ++ ) : ?>
 					<?php $watch = $info->watchlist[ $i ]; ?>
 					<a target="_blank" href="<?= $info->baseUrl . $watch->link ?>"
 					   title="<?= $watch->title ?>" class="imdb-block-item-title">
-							<img src="<?= $watch->logo ?>" class="imdb-block-item-logo"/>
+						<img src="<?= $watch->logo ?>" class="imdb-block-item-logo"/>
 					</a>
-			<?php endfor; ?>
+				<?php endfor; ?>
 
-			<a href="<?= $info->watchlistUrl ?>" target="_blank"
-			   title="See more" class="imdb-widget-small-link">
+				<a href="<?= $info->watchlistUrl ?>" target="_blank"
+				   title="See more" class="imdb-widget-small-link">
 					See all »
-			</a>
-        </div>
+				</a>
+			</div>
 		<?php } ?>
 
-        <!-- LISTS -->
-		<?php if (count( $info->userLists )) { ?>
-        <div class="imdb-user-lists">
-			<div class="imdb-block-title">Lists</div>
-			<?php for ( $i = 0; $i < count( $info->userLists ); $i ++ ) : ?>
+		<!-- LISTS -->
+		<?php if ( count( $info->userLists ) ) { ?>
+			<div class="imdb-user-lists">
+				<div class="imdb-block-title">Lists</div>
+				<?php for ( $i = 0; $i < count( $info->userLists ); $i ++ ) : ?>
 					<?php $list = $info->userLists[ $i ]; ?>
 					<a target="_blank" href="<?= $info->baseUrl . $list->link ?>"
 					   title="List: <?= $list->title ?>" class="imdb-block-item-title">
-							<img src="<?= (substr($list->logo,0,1) == '/' ? $info->baseUrl : '') . $list->logo ?>" 
-								 class="imdb-block-item-small-logo" />
-							<?= $list->title ?>
+						<img src="<?= ( substr( $list->logo, 0, 1 ) == '/' ? $info->baseUrl : '' ) . $list->logo ?>"
+						     class="imdb-block-item-small-logo"/>
+						<?= $list->title ?>
 					</a>
 
-			<?php endfor; ?>
+				<?php endfor; ?>
 
-			<a href="<?= $info->listsUrl ?>" target="_blank"
-			   title="See more" class="imdb-widget-small-link">
+				<a href="<?= $info->listsUrl ?>" target="_blank"
+				   title="See more" class="imdb-widget-small-link">
 					See all »
-			</a>
-        </div>
+				</a>
+			</div>
 		<?php } ?>
 
-        <!-- REVIEWS -->
-        <?php if (count( $info->reviews )) { ?>
-        <div class="imdb-user-reviews">
-			<div class="imdb-block-title">Reviews</div>
-			<?php for ( $i = 0; $i < count( $info->reviews ); $i ++ ) : ?>
-				<?php $review = $info->reviews[ $i ]; ?>
-				 <div class="imdb-block-item-title">
-					<a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>" target="_blank">
-						 <span title="<?= $review->movieTitle ?> <?= $review->movieYear ?>"><?= $review->movieTitle ?></span>
-					</a><?= $review->movieYear ?>
-				</div>
-				<div class="imdb-user-review-title"><?= $review->title ?></div>       
-				<div class="imdb-user-review">                                                  
-					<div class="imdb-user-reviews-left">
-					   <a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>"
-						  class="imdb-block-item-title" target="_blank">
-							<img src="<?= $review->movieLogo ?>" style="" />
-					   </a>
+		<!-- REVIEWS -->
+		<?php if ( count( $info->reviews ) ) { ?>
+			<div class="imdb-user-reviews">
+				<div class="imdb-block-title">Reviews</div>
+				<?php for ( $i = 0; $i < count( $info->reviews ); $i ++ ) : ?>
+					<?php $review = $info->reviews[ $i ]; ?>
+					<div class="imdb-block-item-title">
+						<a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>"
+						   target="_blank">
+							<span
+								title="<?= $review->movieTitle ?> <?= $review->movieYear ?>"><?= $review->movieTitle ?></span>
+						</a><?= $review->movieYear ?>
 					</div>
-					<div class="imdb-user-reviews-right">
-						<span><?= $review->text ?></span>                                
-						<span><?= $review->meta ?></span>
+					<div class="imdb-user-review-title"><?= $review->title ?></div>
+					<div class="imdb-user-review">
+						<div class="imdb-user-reviews-left">
+							<a title="<?= $review->movieTitle ?>" href="<?= $info->baseUrl . $review->movieLink ?>"
+							   class="imdb-block-item-title" target="_blank">
+								<img src="<?= $review->movieLogo ?>" style=""/>
+							</a>
+						</div>
+						<div class="imdb-user-reviews-right">
+							<span><?= $review->text ?></span>
+							<span><?= $review->meta ?></span>
+						</div>
 					</div>
-				</div>
-			<?php endfor; ?>
-			<a href="<?= $info->commentsindexUrl ?>" target="_blank"
-			   title="See all reviews" class="imdb-widget-small-link">
+				<?php endfor; ?>
+				<a href="<?= $info->commentsindexUrl ?>" target="_blank"
+				   title="See all reviews" class="imdb-widget-small-link">
 					See all »
-			</a>
-        </div>
-        <?php } ?>
-		
-		<!-- BOARDS -->
-		<?php if (count( $info->boards )) { ?>
-        <div class="imdb-user-board-messages">
-			<div class="imdb-block-title">Board posts</div>
-			<?php for ( $i = 0; $i < count( $info->boards ); $i ++ ) : ?>
-				<?php $board = $info->boards[ $i ]; 
-				   if (! empty($board->commentTitle)) : ?>
-				<div class="imdb-user-board-message">
-					<a href='<?= $info->baseUrl . $board->boardLink ?>' target="_blank" class="imdb-block-item-title">
-						<?= $board->boardTitle ?>
-					</a>
-					<a href='<?= $info->baseUrl . $board->commentLink ?>' target="_blank" 
-					   title="<?= $board->when ?>" class="imdb-user-board-message-comment">
-						<?= $board->commentTitle ?>
-					</a>
-				</div>
-				<?php endif; ?>
-			<?php endfor; ?>
-			<a href="<?= $info->boardsUrl ?>/" target="_blank"
-			   title="See all board messages" class="imdb-widget-small-link">
-					See all messages »
-			</a>
-        </div>
-        <?php } ?>
-		
+				</a>
+			</div>
+		<?php } ?>
 
-        <!-- OVERLAY RATINGS -->
-        <div class="imdb-widget-charts">
+		<!-- BOARDS -->
+		<?php if ( count( $info->boards ) ) { ?>
+			<div class="imdb-user-board-messages">
+				<div class="imdb-block-title">Board posts</div>
+				<?php for ( $i = 0; $i < count( $info->boards ); $i ++ ) : ?>
+					<?php $board = $info->boards[ $i ];
+					if ( ! empty( $board->commentTitle ) ) : ?>
+						<div class="imdb-user-board-message">
+							<a href='<?= $info->baseUrl . $board->boardLink ?>' target="_blank"
+							   class="imdb-block-item-title">
+								<?= $board->boardTitle ?>
+							</a>
+							<a href='<?= $info->baseUrl . $board->commentLink ?>' target="_blank"
+							   title="<?= $board->when ?>" class="imdb-user-board-message-comment">
+								<?= $board->commentTitle ?>
+							</a>
+						</div>
+					<?php endif; ?>
+				<?php endfor; ?>
+				<a href="<?= $info->boardsUrl ?>/" target="_blank"
+				   title="See all board messages" class="imdb-widget-small-link">
+					See all messages »
+				</a>
+			</div>
+		<?php } ?>
+
+
+		<!-- OVERLAY RATINGS -->
+		<div class="imdb-widget-charts">
 			<span class="imdb-widget-charts-close" title="Close">x</span>
 
 			<div class="imdb-block-title">Ratings charts for <?= $info->nick ?></div>
-			
-				<div class="imdb-widget-chart">
-					<h4>Ratings distribution</h4>
-					<div class="imdb-histogram-horizontal">
-						<?= $info->ratingsDistribution ?>
-					</div>
+
+			<div class="imdb-widget-chart">
+				<h4>Ratings distribution</h4>
+
+				<div class="imdb-histogram-horizontal">
+					<?= $info->ratingsDistribution ?>
 				</div>
-			
-				<div class="imdb-widget-chart">
-					<h4>By year</h4>
-					<div class="imdb-histogram-horizontal">
-						<?= $info->ratingsByYear ?>
-					</div>
-					<div class="imdb-histogram-by-year-legend">
-						<?= $info->ratingsByYearLegend ?>
-					</div>
+			</div>
+
+			<div class="imdb-widget-chart">
+				<h4>By year</h4>
+
+				<div class="imdb-histogram-horizontal">
+					<?= $info->ratingsByYear ?>
 				</div>
-			
-				<div class="imdb-widget-chart">
-					<h4>Top-Rated Genres</h4>
-					<div class="imdb-histogram-by-genre imdb-histogram-vertical">
-						<?= $info->ratingsTopRatedGenres ?>
-					</div>
+				<div class="imdb-histogram-by-year-legend">
+					<?= $info->ratingsByYearLegend ?>
 				</div>
-			
-				<div class="imdb-widget-chart">
-					<h4>Top-Rated Years</h4>
-					<div class="imdb-histogram-vertical">
-						<?= $info->ratingsTopRatedYears ?>
-					</div>
+			</div>
+
+			<div class="imdb-widget-chart">
+				<h4>Top-Rated Genres</h4>
+
+				<div class="imdb-histogram-by-genre imdb-histogram-vertical">
+					<?= $info->ratingsTopRatedGenres ?>
+				</div>
+			</div>
+
+			<div class="imdb-widget-chart">
+				<h4>Top-Rated Years</h4>
+
+				<div class="imdb-histogram-vertical">
+					<?= $info->ratingsTopRatedYears ?>
 				</div>
 			</div>
 		</div>
-    </div>
+	</div>
 
 </aside>
