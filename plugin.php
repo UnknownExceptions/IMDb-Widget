1cb8ce3646e5908f45b5650ad3451c733c50e4c4
Henrique Dias <hacdias@gmail.com>
2015-05-24 19:19:50 +0100

    resolve #22

--- plugin.php
+++ plugin.php
@@ -11,8 +11,8 @@
  *
  * IMDB Widget for WordPress
  *
- *     Copyright (C) 2015 Henrique Dias <hacdias@gmail.com>
- *     Copyright (C) 2015 Luís Soares <lsoares@gmail.com>
+ *     Copyright (C) 2015 Henrique Dias     <hacdias@gmail.com>
+ *     Copyright (C) 2015 Luís Soares       <lsoares@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
