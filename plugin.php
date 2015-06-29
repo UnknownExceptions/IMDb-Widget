498d7051c5e0af6a57c4ed6caf600d76d5764baa
Henrique Dias <hacdias@gmail.com>
2015-06-29 17:42:38 +0100

    v1.0.8

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: IMDb Profile Widget
  * Description: This is a plugin that shows your IMDd profile with a simple widget.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: Henrique Dias and Luís Soares (imdb)
  * Author URI: https://github.com/imdb
  * Network: true
