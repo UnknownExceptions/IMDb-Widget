ec41eacc5ffa21ee80c3c9242a0078b7e00ac7cf
Henrique Dias <hacdias@gmail.com>
2016-03-28 13:21:06 +0100

    fix exploit on pic.php

--- pic.php
+++ pic.php
@@ -1,4 +1,11 @@
 <?php
 
-header( 'Content-Type: image/jpeg' );
-readfile( $_GET["url"] );
+$url = parse_url($_GET["url"]);
+
+if($url['host'] != 'ia.media-imdb.com') {
+  header('HTTP/1.0 406 Not Acceptable');
+  exit();
+}
+
+header('Content-Type: image/jpeg');
+readfile($_GET["url"]);
